90fd3bc0aef1419f3821aa4d16558d85859e133d
larac25 <34246312+larac25@users.noreply.github.com>
2018-12-10 00:05:17 +0100

    navigation in frontend + wip search

--- source/app.py
+++ source/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Request
 from flask_bootstrap import Bootstrap
 
 import vi_twitter.search as search
+from flask.globals import request
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -14,12 +15,14 @@ def index():
 def convo(tweetID):
     return 'A viTweet conversation page showing the conversation starting from the Tweet with ID {}'.format(tweetID)
 
-@app.route('/convo/<tweetID>/linear')
+@app.route('/convo/<tweetID>/linear', methods=['GET', 'POST'])
 def conversation(tweetID):
     # Call function with TWEET-ID + max. Replies (please don't call over 10!)
     # content -> Temporary String (nothing for future)
-    # response -> Dictionary to be used 
-    content, response = search.get_replies(1069660787722084352, 5)
+    # response -> Dictionary to be used
+    if request.method == 'POST':
+        response = request.form['response']
+    content, response = search.get_replies(response, 5)
     return convo(tweetID) + render_template('conversation.html',response=response)
 
 
