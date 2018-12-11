963acfd59800c375cd152025ef8ab7cfc8fa4dd1
Elli <ereuhl@smail.uni-koeln.de>
2018-12-11 15:52:58 +0100

    more routing and language parameter

--- source/app.py
+++ source/app.py
@@ -7,23 +7,25 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
-def get_replies(tweetID, language, maxReplies):
-    # Call function with TWEET-ID + max. Replies (please don't call over 10!)
-    # response -> Dictionary to be used 
-    response = search.get_replies(tweetID, language, maxReplies)
-    return response
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/conversation', methods=['GET', 'POST'])
 def conversation():
     if request.method == 'POST':
         # maybe TODO: check if input is valid (only Twitter URL or ID accepted); if URL, convert to ID
         requestedTweetID = request.form.get('tweetID')
-        language = 'de' #request.form.get('langopt')
-    return render_template('conversation.html',response=get_replies(requestedTweetID, language, 10))
+        requestedLanguage = request.form.get('language')
+    return render_template('conversation.html',response=search.get_replies(requestedTweetID, requestedLanguage, maxReplies=10))
 
 
 if __name__ == "__main__":
