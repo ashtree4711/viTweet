d55d0daf5d930374d5ee20242e01839b04c9ea1d
MonicaZanetta <45204073+MonicaZanetta@users.noreply.github.com>
2018-12-21 17:20:05 +0100

    neue version

--- source/app.py
+++ source/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_bootstrap import Bootstrap
 
 import vi_twitter.search as search
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 Bootstrap(app)
-
 
 @app.route('/')
 def index():
@@ -22,11 +22,15 @@ def contact():
 @app.route('/conversation', methods=['GET', 'POST'])
 def conversation():
     if request.method == 'POST':
-        # maybe TODO: check if input is valid (only Twitter URL or ID accepted); if URL, convert to ID
         requestedTweetID = request.form.get('tweetID')
         requestedLanguage = 'de' #request.form.get('language')
-    return render_template('conversation.html',response=search.get_replies(requestedTweetID, requestedLanguage, 10))
-
+        
+        if len(requestedTweetID) != 19:
+            flash('Invalid ID, please try again!')
+            return redirect(url_for('index'))
+        else: 
+            return render_template('conversation.html', response=search.get_replies(requestedTweetID, requestedLanguage, 10))
+    return render_template('conversation.html', response=search.get_replies(requestedTweetID, requestedLanguage, 10))
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
