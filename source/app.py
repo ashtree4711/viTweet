b3f54d5bd691a99c4bb8112a7ecbe0ecb72d5bcf
MonicaZanetta <45204073+MonicaZanetta@users.noreply.github.com>
2019-02-19 19:33:08 +0100

    network

--- source/app.py
+++ source/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_bootstrap import Bootstrap
-from vi_twitter.search import get_conversation
 import vi_twitter.search as search
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -38,23 +37,34 @@ def conversation():
 
 @app.route('/network', methods=['GET', 'POST'])
 def network():
-    requestedTweetID = request.form.get('tweetID')
-    language = request.form.get('langopt')
-    G = nx.star_graph(7)
-    pos = nx.spring_layout(G)
-    colors = range(7)
-    nx.draw_networkx_nodes(G, pos,nodelist=search.get_conversation(requestedTweetID, language, max_replies= 200),node_color='y',node_size=1000,alpha=0.8)
-    nx.draw_networkx_nodes(G, pos,nodelist=search.get_conversation(requestedTweetID, language, max_replies= 200),node_color='r',node_size=500,alpha=0.8)
-    nx.draw_networkx_nodes(G, pos,nodelist=search.get_conversation(requestedTweetID, language, max_replies= 200),node_color='b',node_size=500,alpha=0.8)
-    # edges 
-    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
-    nx.draw_networkx_edges(G, pos,edgelist=[('1.tweet','2.replies')],width=8, alpha=0.5, edge_color='r')
-    nx.draw_networkx_edges(G, pos,edgelist=[('1.tweet','3.quote_tweets')],width=8, alpha=0.5, edge_color='b')
-    labels = {}
-    nx.draw_networkx_labels(G, pos, labels, font_size=16)
-    plt.axis('off')
-    plt.savefig('network.png')
-    return render_template('network.html', name = plt.show(), url ='network.png')   
+    if request.method == 'POST':
+        twitterSession = request.form.get('tweetID') 
+        requestedTweetID= request.form.get('tweetID')
+        language = request.form.get('langopt')
+        num = len(search.get_conversation(requestedTweetID, language, max_replies=200))
+        G = nx.star_graph(num)
+        pos = nx.spring_layout(G)
+        colors = range(num)
+        nx.draw_networkx_nodes(G, pos,nodelist= [1],node_color='y',node_size=1000,alpha=0.8)
+        list_of_replies = []
+        for i in search.get_conversation(requestedTweetID, language, max_replies=200):
+            list_of_replies.append(i)  
+            nx.draw_networkx_nodes(G, pos,nodelist= int(list_of_replies[i]), node_color='r',node_size=500,alpha=0.8)
+        list_of_quotetweets = []
+        for j in search.get_quote_tweets(twitterSession, requestedTweetID, language):
+            list_of_quotetweets.append(j)
+            nx.draw_networkx_nodes(G, pos,nodelist= int(list_of_quotetweets[i]),node_color='b',node_size=500,alpha=0.8)
+        # edges 
+        nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+        connection1 = zip(requestedTweetID,list_of_replies)
+        connection2 = zip(requestedTweetID, list_of_quotetweets)
+        nx.draw_networkx_edges(G, pos,edgelist=connection1,width=8, alpha=0.5, edge_color='r')
+        nx.draw_networkx_edges(G, pos,edgelist= connection2,width=8, alpha=0.5, edge_color='b')
+        labels = {}
+        nx.draw_networkx_labels(G, pos, labels, font_size=16)
+        plt.axis('off')
+        plt.savefig('network.png')
+        return render_template('network.html', name = plt.show(), url ='network.png')   
 
 
 if __name__ == "__main__":
