69c915db617373f77834cce5414d1c442007cc61
larac25 <34246312+larac25@users.noreply.github.com>
2019-03-11 13:02:56 +0100

    doppelte files gelöscht

--- source/static/response.js
+++ source/static/response.js
@@ -2,10 +2,10 @@
  * Created by Lara on 26.02.19.
  */
 
-//function loading() {
- //   $("#loading").show();
+function loading() {
+   $("#loading").show();
     
-//}
+}
 
 function showReplies() {
     alert("test");
